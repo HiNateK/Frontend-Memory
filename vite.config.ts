156a2d2886d7c6b3b6e4bda1239227a42d0d848b
MacBookPro <Hawaii@MacBookPros-MacBook-Pro.local>
2025-01-15 07:58:33 -1000

    edit vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // Root base for custom domain like https://kinscreen.com
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: [], 
   },
 });
