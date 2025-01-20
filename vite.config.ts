40e891171afd2315b0f34e1dc6d4444a470fbf8d
MacBookPro <Hawaii@MacBookPros-MacBook-Pro.local>
2025-01-20 10:45:29 -1000

    back

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { dirname, resolve } from 'node:path';
-import { fileURLToPath } from 'node:url';
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   base: '/', // Root base for custom domain like https://kinscreen.com
   plugins: [react()],
   optimizeDeps: {
     exclude: [], 
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        dist: resolve(__dirname, 'dist/index.html'),
-      },
-    },
   },
 });
