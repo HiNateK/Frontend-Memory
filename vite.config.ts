2bee749d940852198d747afd67be8fb3c3c2f805
MacBookPro <Hawaii@MacBookPros-MacBook-Pro.local>
2025-01-20 16:37:05 -1000

    back

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/*', // Root base for custom domain like https://kinscreen.com
+  base: '/', // Root base for custom domain like https://kinscreen.com
   plugins: [react()],
   optimizeDeps: {
     exclude: [], 
